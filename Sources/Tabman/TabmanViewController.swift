4b720aafe8ac64492d1da9a0a4cd123c680f11f9
Merrick Sapsford <merricks@apadmi.com>
2017-02-17 13:38:34 +0000

    Add project

---
+++ Sources/Tabman/TabmanViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TabmanViewController.swift
+//  Tabman
+//
+//  Created by Merrick Sapsford on 17/02/2017.
+//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//
+
+import UIKit
+import Pageboy
+
+class TabmanViewController: PageboyViewController {
+    
+}
