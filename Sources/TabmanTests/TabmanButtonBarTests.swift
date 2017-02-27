1c8a0125f421887c01c1efb973c84bac001d5ff8
Merrick Sapsford <merricks@apadmi.com>
2017-02-27 11:11:02 +0000

    Add additional tests

---
+++ Sources/TabmanTests/TabmanButtonBarTests.swift
@@ -0,0 +1,15 @@
+//
+//  TabmanButtonBarTests.swift
+//  Tabman
+//
+//  Created by Merrick Sapsford on 27/02/2017.
+//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//
+
+import XCTest
+@testable import Tabman
+import Pageboy
+
+class TabmanButtonBarTests: XCTestCase {
+
+}
