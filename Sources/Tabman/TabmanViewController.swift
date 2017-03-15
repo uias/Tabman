753296c82495478956b39436c405786afb040360
Merrick Sapsford <merricks@apadmi.com>
2017-03-15 15:58:45 +0000

    Add ability to attach external TabmanBar

--- Sources/Tabman/TabmanViewController.swift
+++ Sources/Tabman/TabmanViewController.swift
@@ -15,8 +15,18 @@ open class TabmanViewController: PageboyViewController, PageboyViewControllerDel
     // MARK: Properties
     //
     
-    /// The Tabman bar.
+    /// The instance managed Tabman bar.
     internal fileprivate(set) var tabmanBar: TabmanBar?
+    /// Currently attached TabmanBar if it exists.
+    internal fileprivate(set) var attachedTabmanBar: TabmanBar?
+    
+    /// Returns the active bar, prefers attachedTabmanBar if available.
+    fileprivate var activeTabmanBar: TabmanBar? {
+        if let attachedTabmanBar = self.attachedTabmanBar {
+            return attachedTabmanBar
+        }
+        return tabmanBar
+    }
     
     /// Configuration for the bar.
     /// Able to set items, appearance, location and style through this object.
@@ -49,8 +59,8 @@ open class TabmanViewController: PageboyViewController, PageboyViewControllerDel
                                       animated: Bool) {
         if animated {
             UIView.animate(withDuration: 0.3, animations: {
-                self.tabmanBar?.updatePosition(CGFloat(index), direction: direction)
-                self.tabmanBar?.layoutIfNeeded()
+                self.activeTabmanBar?.updatePosition(CGFloat(index), direction: direction)
+                self.activeTabmanBar?.layoutIfNeeded()
             })
         }
     }
@@ -59,7 +69,7 @@ open class TabmanViewController: PageboyViewController, PageboyViewControllerDel
                                       didScrollToPageAtIndex index: Int,
                                       direction: PageboyViewController.NavigationDirection,
                                       animated: Bool) {
-        self.tabmanBar?.updatePosition(CGFloat(index),
+        self.activeTabmanBar?.updatePosition(CGFloat(index),
                                     direction: direction)
     }
     
@@ -68,7 +78,7 @@ open class TabmanViewController: PageboyViewController, PageboyViewControllerDel
                                       direction: PageboyViewController.NavigationDirection,
                                       animated: Bool) {
         if !animated {
-            self.tabmanBar?.updatePosition(pageboyViewController.navigationOrientation == .horizontal ? position.x : position.y,
+            self.activeTabmanBar?.updatePosition(pageboyViewController.navigationOrientation == .horizontal ? position.x : position.y,
                                      direction: direction)
         }
     }
@@ -89,9 +99,7 @@ internal extension TabmanViewController {
     ///
     /// - Parameter style: The new style.
     func reloadBar(withStyle style: TabmanBar.Style) {
-        guard let barType = style.rawType else {
-            return
-        }
+        guard let barType = style.rawType else { return }
         
         // re create the tab bar with a new style
         let bar = barType.init()
@@ -153,15 +161,41 @@ public extension TabmanViewController {
     ///
     /// - Parameter bar: The bar to attach.
     public func attach(bar: TabmanBar) {
-        // TODO
+        guard self.attachedTabmanBar == nil else { return }
+        
+        self.tabmanBar?.isHidden = true
+        
+        // hook up new bar
+        bar.dataSource = self
+        bar.delegate = self
+        bar.transitionStore = self.barTransitionStore
+        if let appearance = self.bar.appearance {
+            bar.appearance = appearance
+        }
+        bar.isHidden = true
+        self.attachedTabmanBar = bar
+        
+        bar.reloadData()
     }
+    
     
     /// Detach a currently attached external TabmanBar.
     /// This will reinstate the TabmanViewController managed instance.
     ///
-    /// - Parameter bar: The bar to detach.
-    public func detach(bar: TabmanBar) {
-        // TODO
+    /// - Returns: The detached bar.
+    @discardableResult public func detachAttachedBar() -> TabmanBar? {
+        guard let bar = self.attachedTabmanBar else { return nil }
+        guard self.attachedTabmanBar === bar else { return nil }
+        
+        bar.dataSource = nil
+        bar.delegate = nil
+        bar.transitionStore = nil
+        bar.isHidden = false
+        self.attachedTabmanBar = nil
+        
+        self.tabmanBar?.reloadData()
+        
+        return bar
     }
 }
 
@@ -189,21 +223,24 @@ extension TabmanViewController: TabmanBarDataSource, TabmanBarDelegate {
 extension TabmanViewController: TabmanBarConfigDelegate {
     
     func config(_ config: TabmanBarConfig, didUpdateStyle style: TabmanBar.Style) {
+        guard self.attachedTabmanBar == nil else { return }
+        
         self.clearUpBar(&self.tabmanBar)
         self.reloadBar(withStyle: style)
         self.updateBar(withLocation: config.location)
     }
     
     func config(_ config: TabmanBarConfig, didUpdateLocation location: TabmanBarConfig.Location) {
+        guard self.attachedTabmanBar == nil else { return }
+
         self.updateBar(withLocation: location)
     }
     
     func config(_ config: TabmanBarConfig, didUpdateAppearance appearance: TabmanBar.Appearance) {
-        self.tabmanBar?.appearance = appearance
+        self.activeTabmanBar?.appearance = appearance
     }
     
     func config(_ config: TabmanBarConfig, didUpdateItems items: [TabmanBarItem]?) {
-        self.tabmanBar?.reloadData()
-        self.tabmanBar?.isHidden = items?.count == 0
+        self.activeTabmanBar?.reloadData()
     }
 }
