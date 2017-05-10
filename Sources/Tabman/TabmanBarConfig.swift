bcc9042aa5cf92f50147f16da47e5b817906f35f
Merrick Sapsford <merricks@apadmi.com>
2017-05-10 20:51:12 +0100

    Remove requiredContentInset property

--- Sources/Tabman/TabmanBarConfig.swift
+++ Sources/Tabman/TabmanBarConfig.swift
@@ -98,15 +98,8 @@ public class TabmanBarConfig: Any {
         }
     }
     
-    /// The content inset required for content underneath the bar.
-    @available(*, deprecated: 0.5.0, message: "Use requiredInsets")
-    public internal(set) var requiredContentInset: UIEdgeInsets = .zero
-    
-    public internal(set) var requiredInsets: TabmanBarInsets = .zero {
-        didSet {
-            self.requiredContentInset = requiredInsets.barInsets
-        }
-    }
+    /// The required insets for the bar.
+    public internal(set) var requiredInsets: TabmanBarInsets = .zero
 }
 
 // MARK: - Additional Style properties for internal use
