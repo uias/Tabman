56751f1f4bad75afd25d5535dba55577757b0807
Cory Imdieke <cory.imdieke@gmail.com>
2017-09-15 05:32:36 -0500

    Added context property to tab bar item (#142)

--- Sources/Tabman/TabmanBar/TabmanBar+Item.swift
+++ Sources/Tabman/TabmanBar/TabmanBar+Item.swift
@@ -19,30 +19,35 @@ public extension TabmanBar {
         public private(set) var title: String?
         /// The image to display for the item.
         public private(set) var image: UIImage?
+		/// Context of the item for external reference.
+		public private(set) var context: Any?
         
         // MARK: Init
         
         /// Create an item with a title.
         ///
         /// - Parameter title: The title to display.
-        public init(title: String) {
+		public init(title: String, context: Any? = nil) {
             self.title = title
+			self.context = context
         }
         
         /// Create an item with an image.
         ///
         /// - Parameter image: Image to display.
-        public init(image: UIImage) {
+        public init(image: UIImage, context: Any? = nil) {
             self.image = image
+			self.context = context
         }
       
         /// Create an item with a title and an image
         ///
         /// - Parameter title: The title to display.
         /// - Parameter image: Image to display.
-        public init(title: String, image: UIImage) {
+        public init(title: String, image: UIImage, context: Any? = nil) {
             self.title = title
             self.image = image
+			self.context = context
         }
     }
 }
