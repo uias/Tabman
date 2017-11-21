dedd73adedf049082bae479e1851d67e3fe65ee4
Merrick Sapsford <merrick@sapsford.tech>
2017-11-21 20:30:30 +0100

    Fix deprecations in tests

---
+++ Sources/TabmanTests/TabmanBarBehaviorTests.swift
@@ -0,0 +1,15 @@
+//
+//  TabmanBarBehaviorTests.swift
+//  TabmanTests
+//
+//  Created by Merrick Sapsford on 21/11/2017.
+//  Copyright © 2017 UI At Six. All rights reserved.
+//
+
+import XCTest
+@testable import Tabman
+import Pageboy
+
+class TabmanBarBehaviorTests: TabmanViewControllerTests {
+
+}
