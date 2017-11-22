470bb06a696dec35e5b39295740c013abb76e7cd
Merrick Sapsford <merrick@sapsford.tech>
2017-11-22 12:27:19 +0100

    Add AutoInsetEngine

---
+++ Sources/Tabman/AutoInset/AutoInsetEngine.swift
@@ -0,0 +1,13 @@
+//
+//  AutoInsetEngine.swift
+//  Tabman
+//
+//  Created by Merrick Sapsford on 22/11/2017.
+//  Copyright © 2017 UI At Six. All rights reserved.
+//
+
+import Foundation
+
+class AutoInsetEngine {
+    
+}
