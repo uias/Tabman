283fd871c35a22857db9e648c6bef642cabe29ed
Iqbal Qasim Khan <iqbal.qasim@tkxel.com>
2017-12-06 16:33:04 +0500

    Remove Comments

--- Sources/Tabman/TabmanBar/Styles/Abstract/TabmanStaticButtonBar.swift
+++ Sources/Tabman/TabmanBar/Styles/Abstract/TabmanStaticButtonBar.swift
@@ -21,9 +21,7 @@ extension UIButton {
         self.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: -imageSize.width, bottom: -(imageSize.height + spacing), right: 0.0)
         let labelString = NSString(string: text)
         let titleSize = labelString.size(withAttributes: [NSAttributedStringKey.font: font])
-        self.imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0.0, bottom: 0.0, right: -titleSize.width /*+ imageSize.width / 2*/)
-        //        let edgeOffset = abs(titleSize.height - imageSize.height) / 2.0;
-        //        self.contentEdgeInsets = UIEdgeInsets(top: edgeOffset, left: 0.0, bottom: edgeOffset, right: 0.0)
+        self.imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0.0, bottom: 0.0, right: -titleSize.width)
     }
 }
 
