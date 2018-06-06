6fa7efde290a1961416072bcdbbca290ae617fd3
Merrick Sapsford <merrick@sapsford.tech>
2018-06-06 16:07:47 -0700

    Fix issue where bar button would not lay out

--- Example/Tabman-Example/TabViewController.swift
+++ Example/Tabman-Example/TabViewController.swift
@@ -26,10 +26,8 @@ class TabViewController: TabmanViewController {
         self.dataSource = self
         
         let bar = addBar(BarViewTemplates.ButtonBar(), at: .top)
-        let items = [BarItem(title: "Test")]
-        bar.populate(with: items) { (button, item) in
-            
-        }
+        let items = [BarItem(title: "Test"), BarItem(title: "Test")]
+        bar.populate(with: items)
     }
 }
 
--- Sources/Tabman/Bar/Buttons/BarButton.swift
+++ Sources/Tabman/Bar/Buttons/BarButton.swift
@@ -14,6 +14,7 @@ open class BarButton: UIControl, LayoutPerformer {
     
     public required init() {
         super.init(frame: .zero)
+        performLayout(in: self)
     }
     
     public override init(frame: CGRect) {
