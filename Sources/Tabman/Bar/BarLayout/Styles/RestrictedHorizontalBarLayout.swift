b56cec509a6e6e0e2ffc674285259db40098054a
Merrick Sapsford <merrick@sapsford.tech>
2018-08-29 20:56:10 +0200

    Add focus area to RestrictedHorizontalBarLayout

--- Sources/Tabman/Bar/BarLayout/Styles/RestrictedHorizontalBarLayout.swift
+++ Sources/Tabman/Bar/BarLayout/Styles/RestrictedHorizontalBarLayout.swift
@@ -86,7 +86,21 @@ open class RestrictedHorizontalBarLayout: BarLayout {
     // MARK: BarViewFocusProvider
     
     open override func focusArea(for position: CGFloat, capacity: Int) -> CGRect {
-        return .zero
+        let range = BarMath.localIndexRange(for: position, minimum: 0, maximum: capacity - 1)
+        guard stackView.arrangedSubviews.count > range.upperBound else {
+            return .zero
+        }
+        
+        let lowerView = stackView.arrangedSubviews[range.lowerBound]
+        let upperView = stackView.arrangedSubviews[range.upperBound]
+        
+        let progress = BarMath.localProgress(for: position)
+        let interpolation = lowerView.frame.interpolate(with: upperView.frame, progress: progress)
+        
+        return CGRect(x: lowerView.frame.origin.x + interpolation.origin.x,
+                      y: 0.0,
+                      width: lowerView.frame.size.width + interpolation.size.width,
+                      height: container.bounds.size.height)
     }
 }
 
