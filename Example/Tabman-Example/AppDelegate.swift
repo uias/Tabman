89e80190b55bd10ee99f8e5e6ca0bc6f49990b5b
Merrick Sapsford <merrick@sapsford.tech>
2018-10-16 13:47:05 +0200

    Only use dark gradients in example

--- Example/Tabman-Example/AppDelegate.swift
+++ Example/Tabman-Example/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Randient
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Only use dark gradients
+        Randient.addFilter(that: { !$0.metadata.isPredominantlyLight })
+        
         return true
     }
 
