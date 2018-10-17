9aa91dc3adca0f494a33ff09b33ba6c01d8d7dd2
Merrick Sapsford <merrick@sapsford.tech>
2018-10-17 18:01:22 +0200

    Improve interactive overlay appearance

--- Example/Tabman-Example/BarInteractiveOverlayView.swift
+++ Example/Tabman-Example/BarInteractiveOverlayView.swift
@@ -34,8 +34,7 @@ final class BarInteractiveOverlayView: UIView {
     // MARK: Properties
     
     let overlayView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
         return view
     }()
     
@@ -80,29 +79,85 @@ final class BarInteractiveOverlayView: UIView {
     
     private func renderInsertionAreas(for bar: TMBar, in viewController: TabmanViewController) {
         
-        let topAreaHeight = viewController.topBarContainer.frame.maxY
+        // top area
+        let topAreaInset: CGFloat
+        let topAreaHeight: CGFloat
+        if viewController.topBarContainer.frame.size.height != 0.0 {
+            topAreaInset = viewController.topBarContainer.frame.maxY
+            topAreaHeight = Defaults.areaButtonHeight
+        } else {
+            topAreaInset = 0.0
+            topAreaHeight = Defaults.areaButtonHeight + viewController.topBarContainer.frame.maxY
+        }
         let topAreaButton = makeAreaButton(for: .top)
         addSubview(topAreaButton)
         topAreaButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             topAreaButton.leadingAnchor.constraint(equalTo: leadingAnchor),
-            topAreaButton.topAnchor.constraint(equalTo: topAnchor, constant: topAreaHeight),
+            topAreaButton.topAnchor.constraint(equalTo: topAnchor, constant: topAreaInset),
             topAreaButton.trailingAnchor.constraint(equalTo: trailingAnchor),
-            topAreaButton.heightAnchor.constraint(equalToConstant: Defaults.areaButtonHeight)
+            topAreaButton.heightAnchor.constraint(equalToConstant: topAreaHeight)
             ])
         topAreaButton.addTarget(self, action: #selector(topAreaButtonPressed(_:)), for: .touchUpInside)
         
-        let bottomAreaHeight = viewController.view.frame.height - viewController.bottomBarContainer.frame.minY
+        // bottom area
+        let bottomAreaInset: CGFloat
+        let bottomAreaHeight: CGFloat
+        if viewController.bottomBarContainer.frame.size.height != 0.0 {
+            bottomAreaInset = viewController.view.frame.height - viewController.bottomBarContainer.frame.minY
+            bottomAreaHeight = Defaults.areaButtonHeight
+        } else {
+            bottomAreaInset = 0.0
+            bottomAreaHeight = Defaults.areaButtonHeight + (viewController.view.frame.height - viewController.bottomBarContainer.frame.minY)
+        }
         let bottomAreaButton = makeAreaButton(for: .bottom)
         addSubview(bottomAreaButton)
         bottomAreaButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             bottomAreaButton.leadingAnchor.constraint(equalTo: leadingAnchor),
-            bottomAnchor.constraint(equalTo: bottomAreaButton.bottomAnchor, constant: bottomAreaHeight),
+            bottomAnchor.constraint(equalTo: bottomAreaButton.bottomAnchor, constant: bottomAreaInset),
             bottomAreaButton.trailingAnchor.constraint(equalTo: trailingAnchor),
-            bottomAreaButton.heightAnchor.constraint(equalToConstant: Defaults.areaButtonHeight)
+            bottomAreaButton.heightAnchor.constraint(equalToConstant: bottomAreaHeight)
             ])
         bottomAreaButton.addTarget(self, action: #selector(bottomAreaButtonPressed(_:)), for: .touchUpInside)
+        
+        // Text
+        let textArea = UIView()
+        addSubview(textArea)
+        textArea.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textArea.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textArea.topAnchor.constraint(equalTo: topAreaButton.bottomAnchor),
+            textArea.trailingAnchor.constraint(equalTo: trailingAnchor),
+            textArea.bottomAnchor.constraint(equalTo: bottomAreaButton.topAnchor)
+            ])
+        
+        let topAreaLabel = makeAreaLabel(For: .top)
+        textArea.addSubview(topAreaLabel)
+        topAreaLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAreaLabel.leadingAnchor.constraint(equalTo: textArea.leadingAnchor),
+            topAreaLabel.trailingAnchor.constraint(equalTo: textArea.trailingAnchor),
+            topAreaLabel.topAnchor.constraint(equalTo: textArea.topAnchor, constant: 8.0)
+            ])
+        
+        let bottomAreaLabel = makeAreaLabel(For: .bottom)
+        textArea.addSubview(bottomAreaLabel)
+        bottomAreaLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomAreaLabel.leadingAnchor.constraint(equalTo: textArea.leadingAnchor),
+            bottomAreaLabel.trailingAnchor.constraint(equalTo: textArea.trailingAnchor),
+            textArea.bottomAnchor.constraint(equalTo: bottomAreaLabel.bottomAnchor, constant: 8.0)
+            ])
+        
+        let promptLabel = makePromptLabel()
+        textArea.addSubview(promptLabel)
+        promptLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            promptLabel.leadingAnchor.constraint(equalTo: textArea.leadingAnchor),
+            promptLabel.trailingAnchor.constraint(equalTo: textArea.trailingAnchor),
+            promptLabel.centerYAnchor.constraint(equalTo: textArea.centerYAnchor)
+            ])
     }
     
     // MARK: Lifecycle
@@ -164,15 +219,30 @@ private extension BarInteractiveOverlayView {
     
     func makeAreaButton(for location: TabmanViewController.BarLocation) -> UIButton {
         let button = UIButton()
-        
-        switch location {
-        case .top:
-            button.setTitle("Top", for: .normal)
-        default:
-            button.setTitle("Bottom", for: .normal)
-        }
-        
         button.backgroundColor = UIColor.white.withAlphaComponent(0.3)
         return button
+    }
+    
+    func makePromptLabel() -> UILabel {
+        let promptLabel = UILabel()
+        promptLabel.text = "Select location"
+        promptLabel.textColor = .white
+        promptLabel.font = UIFont.systemFont(ofSize: 26, weight: .regular)
+        promptLabel.textAlignment = .center
+        return promptLabel
+    }
+    
+    func makeAreaLabel(For location: TabmanViewController.BarLocation) -> UILabel {
+        let areaLabel = UILabel()
+        switch location {
+        case .top:
+            areaLabel.text = "Top"
+        default:
+            areaLabel.text = "Bottom"
+        }
+        areaLabel.textColor = .white
+        areaLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        areaLabel.textAlignment = .center
+        return areaLabel
     }
 }
