02a5688dd5b3b580750c4c1c61a808fbc303e5aa
Merrick Sapsford <merrick@sapsford.tech>
2018-11-26 14:02:08 -0500

    Update docs

--- Sources/Tabman/Bar/TMBar+Templates.swift
+++ Sources/Tabman/Bar/TMBar+Templates.swift
@@ -10,7 +10,12 @@ import Foundation
 
 public extension TMBar {
  
+    /// The default button bar, very reminiscent of the Android `FragmentPagerAdapter`. It consists
+    /// of a horizontal layout containing label bar buttons, and a line indicator at the bottom.
     public typealias ButtonBar = TMBarView<TMHorizontalBarLayout, TMLabelBarButton, TMLineBarIndicator>
+    /// iOS 'UITabBar' style bar, featuring a constrained horizontal layout, tab item bar buttons with a
+    /// vertically aligned image and label, and no visible indicator.
     public typealias TabBar = TMBarView<TMConstrainedHorizontalBarLayout, TMTabItemBarButton, TMBarIndicator.None>
+    /// Bar which features only a line indicator, and no buttons.
     public typealias LineBar = TMBarView<TMBarLayout.None, TMBarButton.None, TMLineBarIndicator>
 }
