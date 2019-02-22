de7fa2c6de5723f05d512875082e1e6dc18740a7
Merrick Sapsford <merrick@sapsford.tech>
2019-02-22 11:48:00 +0100

    Add appearance properties to badge view

--- Sources/Tabman/Bar/BarButton/Badge/TMBadgeView.swift
+++ Sources/Tabman/Bar/BarButton/Badge/TMBadgeView.swift
@@ -13,12 +13,47 @@ open class TMBadgeView: UIView {
     // MARK: Defaults
     
     private struct Defaults {
-        static let contentInset = UIEdgeInsets.zero
+        static let contentInset = UIEdgeInsets(top: 4.0, left: 4.0, bottom: 4.0, right: 4.0)
+        static let font = UIFont.systemFont(ofSize: 14, weight: .bold)
     }
     
     // MARK: Properties
     
     private let label = UILabel()
+    
+    open var value: String? {
+        set {
+            label.text = newValue
+        } get {
+            return label.text
+        }
+    }
+    open var attributedValue: NSAttributedString? {
+        set {
+            label.attributedText = newValue
+        } get {
+            return label.attributedText
+        }
+    }
+    open var font: UIFont {
+        set {
+            label.font = newValue
+        } get {
+            return label.font
+        }
+    }
+    open var textColor: UIColor {
+        set {
+            label.textColor = newValue
+        } get {
+            return label.textColor
+        }
+    }
+    open override var tintColor: UIColor! {
+        didSet {
+            backgroundColor = tintColor
+        }
+    }
     
     // MARK: Init
     
@@ -49,8 +84,10 @@ open class TMBadgeView: UIView {
             ])
         
         label.textAlignment = .center
+        label.font = Defaults.font
+        backgroundColor = tintColor
+        
         label.text = "1"
-        backgroundColor = .red
     }
     
     // MARK: Lifecycle
