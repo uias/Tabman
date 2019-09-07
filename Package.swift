ef1617bac453ede6c55e9248922fac6986501aa8
Merrick Sapsford <merrick@sapsford.tech>
2019-09-07 21:56:17 +0200

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tabman",
+            dependencies: ["Pageboy", "AutoInsetter"],
             path: ".",
             sources: ["Sources/Tabman"]),
         .testTarget(
