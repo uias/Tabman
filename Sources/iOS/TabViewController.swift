0f1fc9387c09f0b7fb6d0d0dda7f9115a08f6b6a
Merrick Sapsford <merrick@sapsford.tech>
2020-10-04 11:12:35 +0200

    Add Tabman implementation

--- Sources/iOS/TabViewController.swift
+++ Sources/iOS/TabViewController.swift
@@ -6,16 +6,58 @@
 //
 
 import UIKit
+import Tabman
+import Pageboy
 
-class TabViewController: UIViewController {
+class TabViewController: TabmanViewController, PageboyViewControllerDataSource, TMBarDataSource {
 
+    // MARK: Properties
+    
+    private lazy var viewControllers: [UIViewController] = [
+        ChildViewController(page: 1),
+        ChildViewController(page: 2),
+        ChildViewController(page: 3),
+        ChildViewController(page: 4),
+        ChildViewController(page: 5)
+    ]
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         view.backgroundColor = .red
+        
+        dataSource = self
+        
+        let bar = TMBarView.ButtonBar()
+        
+        bar.layout.contentInset = UIEdgeInsets(top: 0.0, left: 16.0, bottom: 4.0, right: 16.0)
+        bar.layout.interButtonSpacing = 24.0
+        bar.indicator.weight = .light
+        bar.indicator.cornerStyle = .eliptical
+        bar.fadesContentEdges = true
+        bar.spacing = 16.0
+        
+        addBar(bar.systemBar(), dataSource: self, at: .top)
     }
 
-
+    // MARK: PageboyViewControllerDataSource
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        nil
+    }
+    
+    // MARK: TMBarDataSource
+    
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        return TMBarItem(title: "Page No. \(index + 1)")
+    }
 }
-
