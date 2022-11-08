8c49c0e4ffbf1cf57e8b40fa3bd94ec141b708e2
Merrick Sapsford <merrick@sapsford.tech>
2022-11-08 13:47:35 +0100

    Remove unneeded compatibility checks

--- Sources/iOS/ButtonBarExampleViewController.swift
+++ Sources/iOS/ButtonBarExampleViewController.swift
@@ -45,9 +45,7 @@ class ButtonBarExampleViewController: TabmanViewController, PageboyViewControlle
         bar.buttons.customize {
             $0.tintColor = UIColor.tabmanForeground.withAlphaComponent(0.4)
             $0.selectedTintColor = .tabmanForeground
-            if #available(iOS 11, *) {
-                $0.adjustsFontForContentSizeCategory = true
-            }
+            $0.adjustsFontForContentSizeCategory = true
         }
         bar.indicator.tintColor = .tabmanForeground
         
