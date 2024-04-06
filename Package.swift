394768444c3b1320efed494077044b3da61d69da
Merrick Sapsford <merrick@sapsford.tech>
2024-04-06 09:03:05 +0100

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version:5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -16,8 +16,6 @@ let package = Package(
         .package(url: "https://github.com/uias/Pageboy", from: "4.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tabman",
             dependencies: ["Pageboy"],
