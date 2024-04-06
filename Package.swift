c8a327db31c8d79bd5e815952e9a8ce152213cea
Merrick Sapsford <merrick@sapsford.tech>
2024-04-06 10:21:54 +0200

    Add support for Privacy Manifest (#628)
    * Add manifest
    
    * Update podspec
    
    * Update Package.swift
    
    * Clean up Package.swift
    
    * Update Pageboy project required version
    
    * Use mac-os14 runner
    
    * Use latest Xcode

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.10
+
 import PackageDescription
 
 let package = Package(
@@ -16,14 +16,12 @@ let package = Package(
         .package(url: "https://github.com/uias/Pageboy", from: "4.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tabman",
             dependencies: ["Pageboy"],
-            path: ".",
-            exclude: ["Sources/Tabman/Tabman.h", "Sources/Tabman/Info.plist"],
-            sources: ["Sources/Tabman"],
+            path: "Sources/Tabman",
+            exclude: ["Tabman.h", "Info.plist", "PrivacyInfo.xcprivacy"],
+            resources: [.process("PrivacyInfo.xcprivacy")],
             linkerSettings: [
                 .linkedFramework("UIKit")
             ]
